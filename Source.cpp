0553be900f5634e6de19d4ecce36a40f062eaa00
douhaku <douhaku1115@gmail.com>
2020-07-23 13:38:22 +0900

    Merge branch 'master' of https://github.com/douhaku1115/HeijouEirian
    # Conflicts:
    #	Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,27 +4,62 @@
 #include <time.h>
 #define SCREEN_WIDTH 13
 #define SCREEN_HEIGHT 13
-#define Alien_MAX 4
 #define POINT 10
+#define SHOT_MAX 5
 
+typedef struct {
+	int x, y;
+	bool state;
+
+}SHOT;
+SHOT shots[SHOT_MAX];
+enum {
+	OFF,
+	ON,
+};
 int point = 0;
 enum {
 	CELL_TYPE_NONE,
 	CELL_TYPE_BLOCK,
 	CELL_TYPE_POINT,
+	CELL_TYPE_SHOT,
 	CELL_TYPE_MAX
+};
+enum {
+	DIRECTION_NORTH,
+	DIRECTION_WEST,
+	DIRECTION_SOUTH,
+	DIRECTION_EAST,
+	DIRECTION_MAX
+};
+enum {
+MAN,
+Alien1,
+Alien2,
+Alien3,
+Alien_MAX
+};
+int directions[][2] = {
+	{0,-1},//DIRECTION_NORTH,
+	{-1,0},//DIRECTION_WEST,
+	{0,1},//DIRECTION_SOUTH,
+	{1,0}//DIRECTION_EAST,
 };
 char cellAA[][2 + 1] = {
 	"　",//CELL_TYPE_NONE,
 	"■",//CELL_TYPE_BLOCK,
-	"・"//CELL_TYPE_POINT
+	"・",//CELL_TYPE_POINT
+	"○"
 	};
 int cells[SCREEN_HEIGHT][SCREEN_WIDTH];
 typedef struct {
 	int x, y;
+	int directions;
 }Alien;
 
 Alien aliens[Alien_MAX];
+
+
 
 int getMonster(int _x, int _y) {
 	for (int i = 0; i < Alien_MAX; i++)
@@ -32,17 +67,17 @@ int getMonster(int _x, int _y) {
 			return i;
 	return -1;
 }
-void setFreePosition(int* pX, int* pY) {
+void setFreePosition(int* pX, int* pY) { //初期エイリアン配置
 	while (1) {
 		int x = rand() % SCREEN_WIDTH;
 		int y = rand() % SCREEN_HEIGHT;
 		switch (cells[y][x]) {
-		case CELL_TYPE_BLOCK:
+		case CELL_TYPE_BLOCK:  //壁ならやり直し
 			break;
 		default:
 		{
 			int monster = getMonster(x, y);
-			if (monster < 0) {
+			if (monster < MAN) {  //パックマンでないならセット
 				*pX = x;
 				*pY = y;
 				return;
@@ -52,7 +87,23 @@ void setFreePosition(int* pX, int* pY) {
 	}
 
 }
-int main() {
+int getShot(int _x, int _y) {
+	for (int i = 0; i < SHOT_MAX; i++)
+		if ((shots[i].x == _x) && (shots[i].y == _y))
+			return i;
+	return -1;
+
+}
+int getFreeShot() {
+	for (int i = 0; i < SHOT_MAX; i++) {
+		if (shots[i].state == 0)
+			return i;
+		return -1;
+	}
+}
+void init() {
+	srand((unsigned int)time(NULL));
+	
 
 	for (int y = 0; y < SCREEN_HEIGHT; y += SCREEN_HEIGHT - 1)
 		for (int x = 0; x < SCREEN_WIDTH; x++) {
@@ -74,29 +125,76 @@ int main() {
 		}
 	//cells[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2] = CELL_TYPE_NONE;
 
-	aliens[0].x = SCREEN_HEIGHT / 2 + 1;
-	aliens[0].y = SCREEN_HEIGHT / 2 + 1;
+	aliens[MAN].x = SCREEN_HEIGHT / 2 + 1;
+	aliens[MAN].y = SCREEN_HEIGHT / 2 + 1;
 
 	for (int i = 1; i < Alien_MAX; i++)
 		setFreePosition(&aliens[i].x, &aliens[i].y);
+}
+void display() {
+	system("cls");
+	for (int y = 0; y < SCREEN_HEIGHT; y++) {
+		for (int x = 0; x < SCREEN_WIDTH; x++) {
+			int shot_M = getShot(x, y);
+			int alien = getMonster(x, y);
+			if (shot_M > 0)
+				printf("→");
+			if (alien < 0)
+				printf(cellAA[cells[y][x]]);
+			else if (alien > MAN)
+				printf("敵");
+
+			else
+				printf("＠");
+		}
+		printf("\n");
+	}
+	printf("point  %d", point);
+
+}
+void gameOver() {
+	aliens[MAN].x = -1;
+	aliens[MAN].y = -1;
+	display();
+	printf("\n GAME OVER");
+	_getch();
+	exit(0);
+}
+void alienMove() {
+	for (int i =MAN+ 1; i < Alien_MAX; i++) {  //Alienの移動
+		int x = aliens[i].x + directions[aliens[i].directions][0];
+		int y = aliens[i].y + directions[aliens[i].directions][1];
+	
+		int alien = getMonster(x, y);
+		if (alien == MAN) {
+			aliens[i].x = x;
+			aliens[i].y = y;
+			gameOver();
+		}
+		else if ((cells[y][x] == CELL_TYPE_BLOCK)//壁だったら変更
+			|| (alien > MAN)) {
+
+			aliens[i].directions = rand() % DIRECTION_MAX;
+		}
+		else {
+			aliens[i].x = x;
+			aliens[i].y = y;
+		}
+	}
+	
+}
+int main() {
+	init();
+	time_t t = time(NULL);
 
 
 	while (1) {
-		system("cls");
-		for (int y = 0; y < SCREEN_HEIGHT; y++) {
-			for (int x = 0; x < SCREEN_WIDTH; x++) {
-				int alien = getMonster(x, y);
-				if (alien < 0)
-					printf(cellAA[cells[y][x]]);
-				else if (alien > 0)
-					printf("敵");
-				else
-					printf("＠");
-			}
-			printf("\n");
+		display();
+		if (time(NULL) > t) {
+			t = time(NULL);
+			display();
 		}
-		printf("point %d", point);
-
+		
 		int x = aliens[0].x;//ｘ座標
 		int y = aliens[0].y;
 
@@ -105,6 +203,15 @@ int main() {
 		case 's':y++; break;
 		case 'a':x--; break;
 		case 'd':x++; break;
+		case 'm':
+			int shot = getFreeShot();
+			if (shot >= 0) {
+				shots[shot].x = aliens[0].x;
+				shots[shot].y = aliens[0].y;
+				shots[shot].state = 1;
+				//printf("→");
+			}break;
+			display();
 		}
 		switch (cells[y][x]) {
 		case CELL_TYPE_BLOCK:
@@ -116,8 +223,8 @@ int main() {
 			aliens[0].x = x;
 			aliens[0].y = y;
 			break;
-	}
-
+		}
+		alienMove();
 
 }
 	
